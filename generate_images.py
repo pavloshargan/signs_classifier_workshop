f88da2aa126db314f9bda91df2ad47602ba85bf4
Pavlo <pavel.shargan@gmail.com>
2020-10-23 19:28:12 +0300

    init commit

---
+++ generate_images.py
@@ -0,0 +1,23 @@
+import cv2
+import time
+import keyboard
+
+cap = cv2.VideoCapture(0)
+count=0
+sign_name = input("Enter sign name:")
+while True:
+    ret, img = cap.read()
+    cv2.startWindowThread()
+    cv2.namedWindow("image")
+    cv2.imshow('image', img)
+    if keyboard.read_key() == "q":
+        break 
+    elif keyboard.read_key() == "s":
+        ret, img = cap.read()
+        count += 1
+        print(str(count)+" "+sign_name)
+        cv2.imwrite('data/'+sign_name+'.'+str(count)+'.png',img)
+    
+    
+
+    
